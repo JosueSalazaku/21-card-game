623c83af6d46aabe85d96fe77daa6db488aef18d
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-10 22:26:50 +0100

    Create deck for of loop

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const deck = [];
-const suits = ['h', 'd', 'c', 's'];
-const rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+
+for (const suit of suits) {
+    for (let i = 0; i < ranks.length; i++) {
+        const card = {
+            suit: suit,
+            rank: ranks[i],
+            value: values[i]
+        };
+        deck.push(card);
+    }
+}
+
+console.log(deck)
+console.log()
