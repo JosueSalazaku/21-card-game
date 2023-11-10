cf5cbfb87d1093c6cc062b26bf8361fcd246c36a
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-10 22:55:33 +0100

    Add game elements and player/dealer elements in script

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 const values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 
+const gameInfo = document.querySelector("#Gameinfo");
+const roundNumber = document.querySelector("#round-number");
+const playerBalance = document.querySelector("#player-balance");
+
+const playerSection = document.querySelector("#player-section");
+const playerCards = document.querySelector("#player-cards");
+const playerScore = document.querySelector("#player-score");
+
+const dealerSection = document.querySelector("#dealer-section");
+const dealerCards = document.querySelector("#dealer-cards");
+const dealerScore = document.querySelector("#dealer-score");
+
+
 for (const suit of suits) {
     for (let i = 0; i < ranks.length; i++) {
         const card = {
@@ -11,8 +24,8 @@ for (const suit of suits) {
             value: values[i]
         };
         deck.push(card);
-    }
-}
+    };
+};
 
 console.log(deck)
 console.log()
