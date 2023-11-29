4d150f3e59b3ed12c3b9143c9fdf782a0661625d
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-29 21:48:46 +0100

    Create deck with for loop and shuffle function

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const deck = [];
 const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const ranks = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
 const values = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
 
 const gameInfo = document.querySelector("#Gameinfo");
@@ -16,30 +30,33 @@ const dealerCards = document.querySelector("#dealer-cards");
 const dealerScore = document.querySelector("#dealer-score");
 
 const gameControls = document.querySelector("#game-controls");
-const hitBtn = document.querySelector("#hit-bitton");
-const standBtn = document.querySelector("#stand-bitton");
-const restartBtn = document.querySelector("#restart-bitton");
+const hitBtn = document.querySelector("#hit-button");
+const standBtn = document.querySelector("#stand-button");
+const restartBtn = document.querySelector("#restart-button");
 
 const outcomeDisplay = document.querySelector("#outcome-display");
 const outcomeMessage = document.querySelector("#outcome-message");
 
-
-for (const suit of suits) {
+function initializeDeck() {
+  for (const suit of suits) {
     for (let i = 0; i < ranks.length; i++) {
-        const card = {
-            suit: suit,
-            rank: ranks[i],
-            value: values[i]
-        };
-        deck.push(card);
-    };
-};
-
-function shuffle(deck) {
-    for (let i = deck.length - 1; i > 0; i--) {
-        const randomShuffle = Math.floor(Math.random() * (deck.length * 1))
+      const card = {
+        suit: suit,
+        rank: ranks[i],
+        value: values[i],
+      };
+      deck.push(card);
     }
+  }
 }
 
-console.log(deck)
-console.log()
+function shuffle() {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+}
+
+shuffle();
+initializeDeck();
+console.log(deck);
